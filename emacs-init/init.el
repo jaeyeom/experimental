e47380def27a039f5d41e3a2bad35a60c209167b
Jaehyun Yeom <jae.yeom@gmail.com>
2014-03-20 08:49:16 -0700

    Do not try-require for already loaded features.

--- emacs-init/init.el
+++ emacs-init/init.el
@@ -133,20 +133,21 @@
   "Attempt to load a library or module. Return true if the
 library given as argument is successfully loaded. If not, instead
 of an error, just add the package to a list of missing packages."
-  (condition-case err
-      ;; protected form
-      (progn
-        (message "Checking for library `%s'..." feature)
-        (if (stringp feature)
-            (load-library feature)
-          (require feature))
-        (message "Checking for library `%s'... Found" feature))
-    ;; error handler
-    (file-error  ; condition
-     (progn
-       (message "Checking for library `%s'... Missing" feature)
-       (add-to-list 'missing-packages-list feature 'append))
-     nil)))
+  (or (and (not (stringp feature)) (featurep feature))
+      (condition-case err
+          ;; protected form
+          (progn
+            (message "Checking for library `%s'..." feature)
+            (if (stringp feature)
+                (load-library feature)
+              (require feature))
+            (message "Checking for library `%s'... Found" feature))
+        ;; error handler
+        (file-error  ; condition
+         (progn
+           (message "Checking for library `%s'... Missing" feature)
+           (add-to-list 'missing-packages-list feature 'append))
+         nil))))
 
 ;;; Package Initialize
 (when (try-require 'package)
