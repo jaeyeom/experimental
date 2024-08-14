5ab2b4ad8aba985006757c5e11e04783e41ccd02
Jaehyun Yeom <jae.yeom@gmail.com>
2024-08-14 00:36:25 -0700

    Fix ansible playbooks for Termux by using shell

--- devtools/setup-dev/ansible/generate_packages.go
+++ devtools/setup-dev/ansible/generate_packages.go
@@ -8,23 +8,23 @@ import (
 )
 
 var packagesTemplate = `---
-- name: Ensure {{.PkgName}} is present
+- name: Ensure {{.Command}} is present
   hosts: all
   tasks:
-    - name: Ensure {{.PkgName}} is present on non-Termux systems
+    - name: Ensure {{.Command}} is present on non-Termux systems
       package:
         name: {{.DebianPkgName}}
         state: present
       when: ansible_env.TERMUX_VERSION is not defined
       become: yes
 
-    - name: Ensure {{.PkgName}} is present on Termux
-      command: command -v {{.PkgName}} || pkg install -y {{.TermuxPkgName}}
+    - name: Ensure {{.Command}} is present on Termux
+      shell: command -v {{.Command}} || pkg install -y {{.TermuxPkgName}}
       when: ansible_env.TERMUX_VERSION is defined
 `
 
 type PackageData struct {
-	PkgName       string
+	Command       string
 	debianPkgName string
 	termuxPkgName string
 }
@@ -33,22 +33,22 @@ func (p PackageData) DebianPkgName() string {
 	if p.debianPkgName != "" {
 		return p.debianPkgName
 	}
-	return p.PkgName
+	return p.Command
 }
 
 func (p PackageData) TermuxPkgName() string {
 	if p.termuxPkgName != "" {
 		return p.termuxPkgName
 	}
-	return p.PkgName
+	return p.Command
 }
 
 var packages = []PackageData{
-	{PkgName: "curl"},
-	{PkgName: "emacs"},
-	{PkgName: "git"},
-	{PkgName: "jq"},
-	{PkgName: "ssh", debianPkgName: "openssh-client", termuxPkgName: "openssh"},
+	{Command: "curl"},
+	{Command: "emacs"},
+	{Command: "git"},
+	{Command: "jq"},
+	{Command: "ssh", debianPkgName: "openssh-client", termuxPkgName: "openssh"},
 }
 
 func main() {
@@ -58,7 +58,7 @@ func main() {
 	}
 
 	for _, pkg := range packages {
-		f, err := os.Create(pkg.PkgName + ".yml")
+		f, err := os.Create(pkg.Command + ".yml")
 		if err != nil {
 			panic(err)
 		}
