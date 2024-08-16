2ef4ebc4b97b8379e294b44a5aceb9717353a2d7
Jaehyun Yeom <jae.yeom@gmail.com>
2024-08-16 13:49:00 -0700

    Add Ansible Playbook all.yml

--- devtools/setup-dev/ansible/ensure.sh
+++ devtools/setup-dev/ansible/ensure.sh
@@ -3,7 +3,13 @@
 # Script ensure.sh runs the provided playbooks with the provided arguments.
 
 if [ -n "$TERMUX_VERSION" ]; then
+    # Ansible remote temp directory is messed up on Termux like
+    # /data/.ansible/tmp, so we need to set it to a writable directory.
     export ANSIBLE_REMOTE_TEMP=$HOME/.ansible/tmp
+
+    # Upgrading pkg is necessary to avoid issues on Termux. Instead of handling
+    # that in Ansible, we do it here.
+    pkg upgrade -y
 fi
 
 # Run playbook with the provided args with the .yml suffix for each arg.
