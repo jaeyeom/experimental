e9c8c1e89a7ecdfa0f3656e48421339f40b649fe
Jaehyun Yeom <jae.yeom@gmail.com>
2025-11-05 16:37:08 -0800

    feat(setup-dev): Add git-delta

--- devtools/setup-dev/ansible/packages_data.go
+++ devtools/setup-dev/ansible/packages_data.go
@@ -8,6 +8,7 @@ var packages = []PackageData{
 	{command: "cmake"},
 	{command: "curl"},
 	{command: "dart"},
+	{command: "delta", brewPkgName: "git-delta", debianPkgName: "git-delta", termuxPkgName: "git-delta"},
 	{command: "emacs", UbuntuPPA: "ppa:ubuntuhandbook1/emacs", brewPkgName: "emacs-plus", brewTap: "d12frosted/emacs-plus", brewOptions: []string{"with-native-comp", "with-dbus", "with-imagemagick"}},
 	{command: "ffmpegthumbnailer"},
 	{command: "fzf"},
